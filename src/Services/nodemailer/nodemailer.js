cfdd7ed3d75d7305196b0a253eeb535d0436d90e
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-05-26 15:36:37 -0400

    Quick commit

--- src/Services/nodemailer/nodemailer.js
+++ src/Services/nodemailer/nodemailer.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const nodemailer = require("nodemailer")
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'Gmail',
     auth: {
         user: "juliscleaningcompany@gmail.com",
         pass: process.env.NODE_MAILER_PASS
